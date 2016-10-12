049759bef0abb74557140c91913528f8301dc916
jlafondCI <jlafond@carouselindustries.com>
2016-10-12 13:55:50 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,11 +43,15 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-file { '/etc/motd':
-ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0644',
-content => "This is my message, there are many like it but this one is mine.\n",
+#file { '/etc/motd':
+#nsure => file,
+#owner => 'root',
+#group => 'root',
+#mode => '0644',
+#content => "This is my message, there are many like it but this one is mine.\n",
+#}
+exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+path => '/usr/bin:/usr/local/bin',
+creates =>'/etc/motd',
 }
 }
