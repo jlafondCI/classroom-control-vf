16134a9e371a70dbb9c8d06cf8dcfe98e175a7e2
jlafondCI <jlafond@carouselindustries.com>
2016-10-13 13:49:38 -0400

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,46 @@
+class nginx {
+package {'nginx':
+ensure => present,
+}
+file {'/var/www':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode=> '0775',
+}
+file {'/var/www/index.html':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode=> '0664',
+source => 'puppet:///modules/nginx/index.html',
+}
+file {'/var/www/nginx.conf':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode=> '0664',
+source => 'puppet:///modules/nginx/nginx.conf',
+require => Package['nginx'],
+notify => Service['nginx'],
+}
+file {'/etc/nginx/conf.d':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode=> '0775',
+}
+file {'/etc/nginx/conf.d/default.conf':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode=> '0664',
+source => 'puppet:///modules/nginx/default.conf',
+require => Package['nginx'],
+notify => Service['nginx'],
+}
+service {'nginx':
+ensure => running,
+enable => true,
+}
+}
