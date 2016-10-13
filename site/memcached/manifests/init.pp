9d8a8f7e350a2a81caba66a0dc3ab2f1194a1192
jlafondCI <jlafond@carouselindustries.com>
2016-10-13 11:54:17 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -8,7 +8,7 @@ owner => 'root',
 group => 'root',
 mode => '0644',
 source => 'puppet:///modules/memcached/memcached',
-require => Package ['memcached'],
+require => Package['memcached'],
 }
 service {'memcached':
 ensure => running,
