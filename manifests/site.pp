dafb89019120b245f7b592e8d7d5cc513544a433
jlafondCI <jlafond@carouselindustries.com>
2016-10-14 08:35:43 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,10 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   # class { 'my_class': }
+  if $::virtual != 'physical' {
+  $vmname = capitalize($::virtual)
+  notify {"This is a ${vmname} virtual machine.":}
+  }
   #include role::classroom
 #file { '/etc/motd':
 #nsure => file,
